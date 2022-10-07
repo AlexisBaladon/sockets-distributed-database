88ee1e6f9d3c58aa040a2c571e0ee1e735d4b3da
Mathias Gabriel Martinez <mathias.gabriel.martinez@fing.edu.uy>
2022-10-07 17:41:22 -0300

    Algunas def agregadas en database y peerHandler + Avances del descubrimiento.

--- descubrimiento.py
+++ descubrimiento.py
@@ -1,36 +1,139 @@
+from xml.dom import ValidationErr
+from src.server.Database import Database
+
+
+import re
+import socket
+import time
+import zlib
 from dtServer import DtServer
+from peerHandler import Peer
+from src.server.AnnounceSocket import sendMsgBroadcast
+from src.server.DiscoverSocket import receiveFromBroadcast
+#from src.util.Utilis import checkIp 
+#from src.util.Utilis import genMsgDatos 
 
-#thread recibe
-def DISCOVER(udp_skt, server: DtServer):
+# Definicion de Constantes #
+SIZE = 1024 # Tamanio del buffer del mensaje
+FORMAT = 'utf-8' # Formato del mensaje
+
+# Thread recibe y establece nueva conexion
+def DISCOVER(server: DtServer):
     while True:
-        print("")
-        #receive bloqueante
+        # Escucha bloqueante por mensaje broadcast de algún peer.
+        (msg, ip) = receiveFromBroadcast(server.descubrimiento_port)
+        # Parsea el mensaje 'ANNOUNCE <port>', devolviendo la lista ['ANNOUNCE', <port>], o None, None.
+        parse_msg = parse_command_ANNOUNCE(msg)
+        # Si ya el primer elemento en esta lista de parseo es "None", es porque el mensaje recibido tiene el formato correcto.
+        if parse_msg[0] is not None:
+            puerto_peer_datos = parse_msg[1]    # Del parseo me interesa unicamente el nro. de puerto, el cual lugo utilizo para establecer comunicacion con el server anunciado para el protocolo datos.
+        
         #actualizar lista de server.
+        server_nuevo = ip + ':' + puerto_peer_datos
+        enc_server_nuevo = server_nuevo.encode()
+        crc_server_nuevo = hex(zlib.crc32(enc_server_nuevo))
+        nuevo_peer = Peer(ip, puerto_peer_datos, str(crc_server_nuevo))
+        server.peers.set_peer(nuevo_peer)
+
+        # Abre una conexión TCP al puerto dado para soportar DATOS.
+        socket_datos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_datos.connect(ip, puerto_peer_datos)
+
+        # Estuve pensando, y es que a lo mejor nos conviene que la clase Server (o DtServer no se) tenga un atributo como una lista para almacenar estos valores, y reutilizarlos en otro lado.
+        recalculados = recalculate_values(server, crc_server_nuevo)
+        deliver_values(recalculados, socket_datos)
     return
 
-#thread envía
-def ANNOUNCE(udp_skt, server: DtServer, broadcast_ip: str, broadcast_port: str):
+# Thread envia
+def ANNOUNCE(server: DtServer):
+    msg = format_command_ANNOUNCE(server.server_port)
     while True:
-        print("")
-        #avisar que existis en broadcast cada 30
-        #time.sleep 30
+        #aviso que existo en broadcast cada 30 segundos
+        sendMsgBroadcast(msg, server.descubrimiento_port)
+        time.sleep(30)
     return
 
-def recalculate_values(server: DtServer, ip: str, port: str):
-    #recalculas valores de server.peers
-
-    return #definir que devolver
+def recalculate_values(server: DtServer, crc_server_nuevo: str):
+    # Declarar una lista de retorno, inicialmente vacia
+    valores_a_transferir = dict()
+    valores_servidor_actual = server.database.get_all()
+    for i in valores_servidor_actual:
+        clave_valor = i + ':' + valores_servidor_actual[i]
+        enc_clave_valor = clave_valor.encode()
+        crc_clave_valor = hex(zlib.crc32(enc_clave_valor))
+        diff1 = abs(int(crc_server_nuevo, 16) - int(crc_clave_valor, 16))
+        diff2 = abs(int(server.firma, 16) - int(crc_clave_valor, 16))
+        if (diff1) <= (diff2):
+            # En caso de haber empate de diferencias, se elige el servidor con la firma más baja como el designado a la clave.
+            if int(crc_server_nuevo, 16) < int(server.firma, 16):
+                # Si la firma le corresponde al nuevo servidor, se prepara la clave y su respectivo valor para ser transferido (ya armado con forma de un mensaje SET de DATOS).
+                msg = 'SET ' + i + ' ' + valores_servidor_actual[i] + '\n'
+                valores_a_transferir[crc_clave_valor] = msg
+            # Si la firma más baja es la de este servidor, entonces no se hace nada (i.e.: conserva la <clave, valor>).
+    return valores_a_transferir
     
-def deliver_values(server: DtServer, ip: str, value: str):
+#def deliver_values(server: DtServer, ip: str, value: str):
+# Esta nueva definición asume que 'socket_abierto_datos' es parte de una conexion TCP ya hecha y abierta.
+def deliver_values(diccionario_recalculados, socket_abierto_datos):
     #se ejecuta despues de recalculate_values
+    for i in diccionario_recalculados:
+        socket_abierto_datos.send(diccionario_recalculados[i].encode(FORMAT))
     return
 
 #Parsing
 def format_command_ANNOUNCE(port: str) -> str:
-    return f"ANNOUNCE {port}"
+    return f"ANNOUNCE {port}\n"
 
-def parse_command_ANNOUNCE(command: str) -> str:
-    #recibe el metodo (announce) y atributos y los convierte a string
-    return
+def parse_command_ANNOUNCE(command: str) -> tuple[str, str]:
+    regex_method = {
+        "ANNOUNCE": r'^ANNOUNCE (\d|\w+)\n$',
+    }
+    for method in regex_method:
+        regex = re.compile(regex_method[method])
+        method_match = regex.match(command)
+        if method_match is not None:
+            value = method_match.group(1)
+            return method, value
+    return None, None
 
+# PARA PRUEBAS
+if __name__ == '__main__':
+#    res = parse_command_ANNOUNCE('ANNOUNCE dfs1234\n')
+#    if res[0] is not None:
+#        print(res)
+#    else:
+#        print('NNOOOUUUPPP')
 
+    mi_server = '192.168.0.100' + ':' + '2022'
+    enc_mi_server = mi_server.encode()
+    crc_mi_server = hex(zlib.crc32(enc_mi_server))
+
+    server_nuevo = '10.1.0.1' + ':' + '2022'
+    enc_server_nuevo = server_nuevo.encode()
+    crc_server_nuevo = hex(zlib.crc32(enc_server_nuevo))
+
+    valores_a_transferir = dict()
+    valores_servidor_actual = Database()
+    valores_servidor_actual.set('clave1', 'valor1')
+    crc_valor1 = hex(zlib.crc32(('clave1:valor1').encode()))
+    print('Firma clave1 (y en decimal): ' + crc_valor1 + ' (' + str(int(crc_valor1, 16)) + ')')
+    valores_servidor_actual.set('clave2', 'valor2')
+    crc_valor2 = hex(zlib.crc32(('clave2:valor2').encode()))
+    print('Firma clave2 (y en decimal): ' + crc_valor2 + ' (' + str(int(crc_valor2, 16)) + ')')
+    valores = valores_servidor_actual.get_all()
+    for i in valores:
+        clave_valor = i + ':' + valores[i]
+        enc_clave_valor = clave_valor.encode()
+        crc_clave_valor = hex(zlib.crc32(enc_clave_valor))
+        if (abs(int(crc_server_nuevo, 16)-int(crc_clave_valor, 16))) < (abs(int(crc_mi_server, 16)-int(crc_clave_valor, 16))):
+            msg = 'SET ' + i + ' ' + valores[i] + '\n'
+            valores_a_transferir[crc_clave_valor] = msg
+    
+    print('Firma server actual (y en decimal): ' + crc_mi_server + ' (' + str(int(crc_mi_server, 16)) + ')')
+    print('Firma server nuevo (y en decimal): ' + crc_server_nuevo + ' (' + str(int(crc_server_nuevo, 16)) + ')')
+
+    print('Valores antes del recalculo...')
+    valores_servidor_actual.show()
+
+    print('Valores que van al nuevo server despues del recalculo...')
+    print(valores_a_transferir)
--- peerHandler.py
+++ peerHandler.py
@@ -7,6 +7,14 @@ class Peer:
         self.crc = crc
         return
 
+    ################################
+    # DE USO EXCLUSIVO PARA PRUEBAS
+    ################################
+    def show(self):
+        print(self.server_IP + '   ' + self.server_port + '   ' + str(self.crc))
+        return
+    ###################################
+
 class PeerHandler:
     def __init__(self, peers: dict = {}):
         self.lock = Lock()
@@ -42,3 +50,59 @@ class PeerHandler:
     #    return
 
     
+    def get_peer(self, crc: str):
+        with self.lock:
+            value = self.peers[crc]
+        return value
+    
+    def get_all(self):
+        with self.lock:
+            value = self.peers
+        return value
+
+    def set_peer(self, peer: Peer):
+        with self.lock:
+            self.peers[peer.crc] = peer
+        return
+
+    def delete_peer(self, crc: str):
+        with self.lock:
+            del self.peers[crc]
+        return
+
+####################
+# MAIN PARA PRUEBAS
+####################
+if __name__ == '__main__':
+    peer1 = Peer('190.120.5.132', '8888', '0xA109F5')
+    peer2 = Peer('120.0.15.111', '37652', '0xABCDEF')
+    peer3 = Peer('10.1.1.5', '342189', '0x784C00')
+    manejador_peers = PeerHandler()
+    try:
+        print('Cargando peers...')
+        manejador_peers.set_peer(peer1)
+        manejador_peers.set_peer(peer2)
+        manejador_peers.set_peer(peer3)
+    except:
+        print('Error al setar peers.')
+    try:
+        print('Mostrando peers...')
+        mis_peers = manejador_peers.get_all()
+        for i in mis_peers:
+            mis_peers[i].show()
+    except:
+        print('Error al mostrar los peers.')
+    try:
+        print('Buscando un peer...')
+        res_per = manejador_peers.get_peer('0xABCDEF')
+        res_per.show()
+    except:
+        print('No fue posible encontrar el peer.')
+    try:
+        print('Borrando un peer...')
+        manejador_peers.delete_peer('0xABCDEF')
+        mis_peers = manejador_peers.get_all()
+        for i in mis_peers:
+            mis_peers[i].show()
+    except:
+        print('Fallo al eliminar el peer...')
